b04ac162e346a96aaf66945e0775908077beb50e
suzana2314 <105118318+suzana2314@users.noreply.github.com>
2025-10-05 16:00:56 +0100

    fix(nix): changed from nil to nixd

--- nvim/ftplugin/nix.lua
+++ nvim/ftplugin/nix.lua
@@ -1,5 +1,5 @@
 -- Exit if the language server isn't available
-if vim.fn.executable('nil') ~= 1 then
+if vim.fn.executable('nixd') ~= 1 then
   return
 end
 
@@ -11,8 +11,15 @@ local root_files = {
 }
 
 vim.lsp.start {
-  name = 'nil_ls',
-  cmd = { 'nil' },
+  name = 'nixd',
+  cmd = { 'nixd' },
   root_dir = vim.fs.dirname(vim.fs.find(root_files, { upward = true })[1]),
   capabilities = require('user.lsp').make_client_capabilities(),
+  settings = {
+    nixd = {
+      formatting = {
+        command = { "nixfmt" },
+      },
+    },
+  },
 }
