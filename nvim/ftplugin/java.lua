137f85969b2e0359f3a24d7c5bbb38c117f2810f
suzana2314 <105118318+suzana2314@users.noreply.github.com>
2025-10-05 17:07:34 +0100

    feat(java): added java language server

---
+++ nvim/ftplugin/java.lua
@@ -0,0 +1,20 @@
+-- Exit if the language server isn't available
+if vim.fn.executable('jdtls') ~= 1 then
+  return
+end
+
+local root_files = {
+  "gradlew",
+  ".gradle",
+  "mvnw",
+  "pom.xml",
+  ".vim/",
+  ".git/"
+}
+
+vim.lsp.start {
+  name = 'jdtls',
+  cmd = { 'jdtls' },
+  root_dir = vim.fs.dirname(vim.fs.find(root_files, { upward = true })[1]),
+  capabilities = require('user.lsp').make_client_capabilities(),
+}
