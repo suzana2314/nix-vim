6781f140783bcf77ef5efe8a919aa7b7ada5774f
suzana2314 <105118318+suzana2314@users.noreply.github.com>
2025-10-05 14:59:24 +0100

    feat(theme): gruvbox theme added

---
+++ nvim/plugin/gruvbox.lua
@@ -0,0 +1,17 @@
+if vim.g.did_load_gruvbox_plugin then
+  return
+end
+vim.g.did_load_gruvbox_plugin = true
+
+require("gruvbox").setup({
+  terminal_colors = true,
+  italic = {
+    strings = false,
+    emphasis = false,
+    comments = false,
+    operators = false,
+    folds = false,
+  },
+  strikethrough = false,
+  contrast = "hard",
+})
