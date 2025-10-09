cacedd20daff0da76a41c1e4d5b72dd7f1a7f24f
suzana2314 <105118318+suzana2314@users.noreply.github.com>
2025-10-09 16:20:00 +0100

    chore: cleanup and reorganize plugin structure

--- nix/neovim-overlay.nix
+++ nix/neovim-overlay.nix
@@ -4,14 +4,6 @@ with final.pkgs.lib;
 let
   pkgs = final;
 
-  # Use this to create a plugin from a flake input
-  mkNvimPlugin =
-    src: pname:
-    pkgs.vimUtils.buildVimPlugin {
-      inherit pname src;
-      version = src.lastModifiedDate;
-    };
-
   # Make sure we use the pinned nixpkgs instance for wrapNeovimUnstable,
   # otherwise it could have an incompatible signature when applying this overlay.
   pkgs-locked = inputs.nixpkgs.legacyPackages.${pkgs.system};
@@ -21,17 +13,13 @@ let
     inherit (pkgs-locked) wrapNeovimUnstable neovimUtils;
   };
 
-  # A plugin can either be a package or an attrset, such as
-  # { plugin = <plugin>; # the package, e.g. pkgs.vimPlugins.nvim-cmp
-  #   config = <config>; # String; a config that will be loaded with the plugin
-  #   # Boolean; Whether to automatically load the plugin as a 'start' plugin,
-  #   # or as an 'opt' plugin, that can be loaded with `:packadd!`
-  #   optional = <true|false>; # Default: false
-  #   ...
-  # }
   all-plugins = with pkgs.vimPlugins; [
+    # syntax
     nvim-treesitter.withAllGrammars
-    luasnip # snippets | https://github.com/l3mon4d3/luasnip/
+
+    # snippets
+    luasnip
+
     # completions
     nvim-cmp
     cmp_luasnip
@@ -43,50 +31,46 @@ let
     cmp-nvim-lua
     cmp-cmdline
     cmp-cmdline-history
-    # git integration plugins
-    gitsigns-nvim # https://github.com/lewis6991/gitsigns.nvim/
-    vim-fugitive # https://github.com/tpope/vim-fugitive/
-    # ^ git integration plugins
 
-    neo-tree-nvim
+    # git
+    gitsigns-nvim
+    vim-fugitive
+
+    # utils
     nvim-autopairs
-    toggleterm-nvim
+    auto-session # saves sessions
+    nvim-unception # prevent nested neovim sessions
+    which-key-nvim
 
     # telescope and extensions
-    telescope-nvim # https://github.com/nvim-telescope/telescope.nvim/
-    telescope-fzy-native-nvim # https://github.com/nvim-telescope/telescope-fzy-native.nvim
+    telescope-nvim
+    telescope-fzy-native-nvim
 
     # UI
-    lualine-nvim # Status line | https://github.com/nvim-lualine/lualine.nvim/
-    statuscol-nvim # Status column | https://github.com/luukvbaal/statuscol.nvim/
+    lualine-nvim # status line
     nvim-treesitter-context # nvim-treesitter-context
     gruvbox-nvim # theme
+    neo-tree-nvim # file explorer
+    toggleterm-nvim # terminal
 
     # navigation/editing enhancement plugins
-    vim-unimpaired # predefined ] and [ navigation keymaps | https://github.com/tpope/vim-unimpaired/
-    eyeliner-nvim # Highlights unique characters for f/F and t/T motions | https://github.com/jinh0/eyeliner.nvim
-    nvim-surround # https://github.com/kylechui/nvim-surround/
-    nvim-treesitter-textobjects # https://github.com/nvim-treesitter/nvim-treesitter-textobjects/
-    nvim-ts-context-commentstring # https://github.com/joosepalviste/nvim-ts-context-commentstring/
-    # ^ navigation/editing enhancement plugins
-    # Useful utilities
-    nvim-unception # Prevent nested neovim sessions | nvim-unception
-    # ^ Useful utilities
-    # libraries that other plugins depend on
+    vim-unimpaired # predefined ] and [ navigation keymaps
+    eyeliner-nvim # highlights unique characters for f/F and t/T motions
+    nvim-treesitter-textobjects
+    nvim-ts-context-commentstring
+
+    # dependencies
     sqlite-lua
     plenary-nvim
     nvim-web-devicons
     vim-repeat
 
-    which-key-nvim
-
   ];
 
   extraPackages = with pkgs; [
-    # language servers, etc.
     lua-language-server
     nixd # nix LSP
-    nixfmt-rfc-style
+    nixfmt-rfc-style # nix formatter
     ripgrep # for telescope
   ];
 in
