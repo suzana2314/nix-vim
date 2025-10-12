1e1b5bb62c7f77294c368cb7208ee2568f6f737f
suzana2314 <105118318+suzana2314@users.noreply.github.com>
2025-10-12 14:28:09 +0100

    chore(java): changed tabstops

--- nvim/ftplugin/java.lua
+++ nvim/ftplugin/java.lua
@@ -18,3 +18,6 @@ vim.lsp.start {
   root_dir = vim.fs.dirname(vim.fs.find(root_files, { upward = true })[1]),
   capabilities = require('user.lsp').make_client_capabilities(),
 }
+
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
